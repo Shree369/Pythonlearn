5ac0750367eac3e1ae4e6d7678d0d7285ec0b391
Shree369 <sonawaneshreyash896@gmail.com>
2021-07-18 21:08:19 +0530

    List file

--- functions/list.py
+++ functions/list.py
@@ -10,5 +10,22 @@
 # ls = ["Shreyash","Mummy",["Pappa","New car"]]
 # print(ls[2][0])
 
-# ls = ["Shreyash","Mummy",["Pappa","New car"]]
+# # List -ive  index
+ls = ["Shreyash","Mummy",["Pappa","New car"]]
 # print(ls[-2])
+# print(ls.index("Shreyash"))
+
+# #append
+# ls.append("Hiii")
+# print(ls)
+
+# #insert
+# ls.insert(2,"Shweta")
+# print(ls)
+
+# #delete
+# del(ls[2])
+# print(ls)
+
+#Scaling
+print(ls[1:3])
